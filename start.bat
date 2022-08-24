45b38980f3c2d1c05c6ab31fd5ff936b4e22dc19
the biggest <111371924+pqtrece@users.noreply.github.com>
2022-08-24 22:24:51 +0200

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,14 @@
+python -m pip install psutil
+python -m pip install requests
+python -m pip install wmi
+python -m pip install pycryptodome
+python -m pip install discord
+python -m pip install discord.py
+python -m pip install pillow
+python -m pip install alive-progress
+python -m pip install tinyaes
+python -m pip install wheel
+python -m pip install pyinstaller
+python -m pip install pypiwin32
+cls
+start python main.py
